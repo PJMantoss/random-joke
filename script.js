76d566c107d2e391040b399be60696b94fc497ff
pjmantoss <joelptoss@gmail.com>
2020-10-25 20:57:44 +0100

    update script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Variables
+let punchline;
 const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
