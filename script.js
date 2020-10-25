89eeec248175c43275238c0b4bc6dcdbddefc090
pjmantoss <joelptoss@gmail.com>
2020-10-25 21:00:40 +0100

    update script

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
+
+//Add event listener for the punchline button. When clicked it should call a function called getPunchline
+/* Create the getPunchline function. This function should: 
+    Insert the punchline into the punchlineDiv
+    Add the class "bubble" to the punchlineDiv
+    Toggle the "hidden" class on both buttons.
+*/
+
+punchlineBtn.addEventListener("click", getPunchline);
+
